8e863c8e854fa60430c18543b7ddd326d960d022
osbon mwangori <71443732+osbon-arch@users.noreply.github.com>
2025-10-13 09:14:34 +0300

    ck editor

--- matara_foundation website/matara_foundation/settings.py
+++ matara_foundation website/matara_foundation/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+CKEDITOR_IMAGE_BACKEND = 'PILLOW'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
